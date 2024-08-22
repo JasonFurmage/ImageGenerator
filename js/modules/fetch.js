bc6f49712e473102534414aae5073caacac85593
JasonFurmage <150832588+JasonFurmage@users.noreply.github.com>
2024-08-22 15:29:59 +0100

    feat: add function for fetching image

--- js/modules/fetch.js
+++ js/modules/fetch.js
@@ -1,3 +1,14 @@
 // ==========================================================================
 // Fetch
 // ==========================================================================
+
+export function fetchImage(url) {
+    return fetch(url)
+             .then(checkStatus)  
+             .then(response => response.url)
+             .catch(error => {
+                console.log('Looks like there was a problem!', error)
+                return Promise.reject(error)
+             })
+}
+
