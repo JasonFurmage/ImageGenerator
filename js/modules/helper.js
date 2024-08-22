9eb2d191564cc043d571a0e03de6bdb4cb3cb0d4
JasonFurmage <150832588+JasonFurmage@users.noreply.github.com>
2024-08-22 16:08:36 +0100

    feat: add function for displaying fetched image

--- js/modules/helper.js
+++ js/modules/helper.js
@@ -1,3 +1,8 @@
 // ==========================================================================
 // Helper
 // ==========================================================================
+
+function generateImage(imgURL) {
+    const generatorImage = document.querySelector('.generator__image');
+    generatorImage.src = imgURL;
+}
