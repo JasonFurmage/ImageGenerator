4b83c9dc2aa9ddb490923a0298b4848770b47f0a
JasonFurmage <150832588+JasonFurmage@users.noreply.github.com>
2024-08-23 16:40:43 +0100

    feat: add function for inserting HTML elements at specific index

--- js/modules/helper.js
+++ js/modules/helper.js
@@ -6,3 +6,14 @@ export function generateImage(imgURL) {
     const generatorImage = document.querySelector('.generator__image');
     generatorImage.src = imgURL;
 }
+
+export function insertElementAtIndex(parent, child, index) {
+
+    const children = Array.from(parent.childNodes);
+
+    if (index === children.length) {
+        parent.appendChild(newElement);
+    } else {
+        parent.insertBefore(newElement, children[index]);
+    }
+}
