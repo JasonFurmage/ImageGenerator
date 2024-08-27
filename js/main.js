495949e08cc3fc34c2f2c1e649aba1213e4037e2
JasonFurmage <150832588+JasonFurmage@users.noreply.github.com>
2024-08-27 15:08:54 +0100

    feat: call adjust library window function on resize event

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@
 // Main
 // ==========================================================================
 
+import { adjustLibraryWindow } from './modules/layout.js';
 import { fetchImage } from './modules/generator.js';
 import { generateImage } from './modules/helper.js';
 
 fetchImage()
     .then(imgURL => generateImage(imgURL))
+
+window.addEventListener('resize', adjustLibraryWindow);
     
