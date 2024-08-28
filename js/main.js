808008c794ba23efc93403402789f0c9a3b014fa
JasonFurmage <150832588+JasonFurmage@users.noreply.github.com>
2024-08-28 19:04:32 +0100

    feat: add id and event listener to next button to fetch new image

--- js/main.js
+++ js/main.js
@@ -4,8 +4,10 @@
 
 import { adjustLibraryWindow } from './modules/layout.js';
 import { fetchImage } from './modules/generator.js';
+import { $nextButton } from './modules/generator.js';
 
 adjustLibraryWindow();
 fetchImage()
 
 window.addEventListener('resize', adjustLibraryWindow);
+$nextButton.addEventListener('click', fetchImage);
