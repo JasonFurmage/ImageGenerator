6bc5239865f88bb48bd4aad978b33b79dd789b4e
JasonFurmage <150832588+JasonFurmage@users.noreply.github.com>
2024-08-29 22:23:30 +0100

    feat: update handleSelectChange function to show and hide new account section

--- js/modules/account.js
+++ js/modules/account.js
@@ -3,7 +3,14 @@
 // ==========================================================================
 
 export const $accountSelect = document.querySelector('#accountSelect');
+export const $accountNew = document.querySelector('#accountNew');
 
 export function handleSelectChange() {
+    const selectedValue = $accountSelect.value;
 
+    if (selectedValue === 'new') {
+        $accountNew.style.display = "block";
+    } else {
+        $accountNew.style.display = "none";
+    }
 }
