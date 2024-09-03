35777a1be9cdba870e678c7fc3351d4c8df4250c
JasonFurmage <150832588+JasonFurmage@users.noreply.github.com>
2024-09-03 14:12:37 +0100

    feat: add regex email validator

--- js/modules/helper.js
+++ js/modules/helper.js
@@ -12,3 +12,8 @@ export function insertElementAtIndex(parent, child, index) {
         parent.insertBefore(child, children[index]);
     }
 }
+
+export function isValidEmail(email) {
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(email);
+  }
