4e63dfe2dec968a266a931709540b63046d99fa9
JasonFurmage <150832588+JasonFurmage@users.noreply.github.com>
2024-09-04 18:08:39 +0100

    feat: add function to update library text

--- js/modules/library.js
+++ js/modules/library.js
@@ -3,6 +3,7 @@
 // ==========================================================================
 
 const $libraryGallery = document.getElementById('libraryGallery');
+const $libraryText = document.getElementById('libraryText');
 
 export function loadLibrary(library) {
     clearLibrary();
@@ -17,8 +18,19 @@ export function insertImage(imgURL) {
     img.src = imgURL;
     img.classList.add('library__gallery-item');
     $libraryGallery.prepend(img);
+    updateLibraryText();
 }
 
 export function clearLibrary() {
     $libraryGallery.innerHTML = '';
+    updateLibraryText();
+}
+
+function updateLibraryText() {
+    if ($libraryGallery.children.length === 1) {
+        $libraryText.textContent = '';
+    } 
+    else if ($libraryGallery.children.length === 0) {
+        $libraryText.textContent = 'No Saved Images';
+    }
 }
