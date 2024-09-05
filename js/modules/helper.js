93541bb5fc36944d39a1791bc6420a5b0eac8b34
JasonFurmage <150832588+JasonFurmage@users.noreply.github.com>
2024-09-05 20:36:19 +0100

    feat: add function for toggling visibility of elements

--- js/modules/helper.js
+++ js/modules/helper.js
@@ -16,4 +16,12 @@ export function insertElementAtIndex(parent, child, index) {
 export function isValidEmail(email) {
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return emailRegex.test(email);
-  }
+}
+
+export function toggleVisibility(element, show) {
+    if (show) {
+        element.classList.remove('hidden');
+    } else {
+        element.classList.add('hidden');
+    }
+}
