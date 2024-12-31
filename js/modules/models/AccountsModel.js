a6b6b3a3801280fccdad9f8eeae56ee92907fcf2
JasonFurmage <150832588+JasonFurmage@users.noreply.github.com>
2024-12-31 01:11:14 +0000

    docs: add accounts model comments

--- js/modules/models/AccountsModel.js
+++ js/modules/models/AccountsModel.js
@@ -7,6 +7,7 @@ export class AccountsModel {
         this.accounts = [];
     }
 
+    // Add new account if validation checks are passed.
     addAccount(email) {
 
         if (!this.#isValidEmail(email)) {
@@ -21,21 +22,25 @@ export class AccountsModel {
         }
     }
 
+    // Use regex to check if email is valid.
     #isValidEmail(email) {
         const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return emailRegex.test(email);
     }
     
+    // Find account with matching email.
     getAccount(email) {
         return this.accounts.find(account => account.email === email);
     }
 
     #Account = class {
+        // Create new account using email.
         constructor(email) {
             this.email = email;
             this.images = [];
         }
     
+        // Add image to array if it does not exist already.
         addImage(image) {
             if (!this.images.includes(image)) {
                 this.images.push(image);
