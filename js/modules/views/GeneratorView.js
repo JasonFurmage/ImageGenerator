37dae0fc3625d22613cfe9bf5377686a424fea29
JasonFurmage <150832588+JasonFurmage@users.noreply.github.com>
2024-12-31 00:39:27 +0000

    docs: add generator view comments

--- js/modules/views/GeneratorView.js
+++ js/modules/views/GeneratorView.js
@@ -3,6 +3,7 @@
 // ==========================================================================
 
 export class GeneratorView {
+    // Get generator elements.
     constructor() {
         this.$generatorImage = document.querySelector('.generator__image');
         this.$generatorNextButton = document.querySelector('.generator__btn.btn.btn--secondary');
