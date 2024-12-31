ecc5433534d0d48078561558f1fcf2c0554adf00
JasonFurmage <150832588+JasonFurmage@users.noreply.github.com>
2024-12-31 00:28:53 +0000

    docs: add account view comments

--- js/modules/views/AccountsView.js
+++ js/modules/views/AccountsView.js
@@ -5,6 +5,7 @@
 import { toggleVisibility } from "../helpers/Helper.js";
 
 export class AccountsView {
+    // Get account elements.
     constructor() {
         this.$accountSelect = document.querySelector('.account__select');
         this.$accountNew = document.querySelector('.account__new');
@@ -13,11 +14,13 @@ export class AccountsView {
         this.$accountCancelButton = document.querySelector('.account__btn.btn.btn--secondary');
     }
 
+    // Switch between showing account select or account new.
     toggleAccountNew(isNew) {
         toggleVisibility(this.$accountNew, isNew);
         toggleVisibility(this.$accountSelect, !isNew);
     }
 
+    // Add new select option and trigger change event.
     addSelectOption(value) {
         const event = new Event('change');
         const newOption = new Option(value, value, undefined, true);
@@ -25,10 +28,12 @@ export class AccountsView {
         this.$accountSelect.dispatchEvent(event);
     }
 
+    // Clear text field.
     clearTextField() {
         this.$accountTextField.value = '';
     }
 
+    // Select first option and trigger change event.
     selectDefault() {
         const event = new Event('change');
         this.$accountSelect.selectedIndex = 0;
