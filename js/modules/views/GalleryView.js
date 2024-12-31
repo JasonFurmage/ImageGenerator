6a1e9cb104722b9e09cd9abc1758079fa8722e6b
JasonFurmage <150832588+JasonFurmage@users.noreply.github.com>
2024-12-31 00:32:09 +0000

    docs: add gallery view comments

--- js/modules/views/GalleryView.js
+++ js/modules/views/GalleryView.js
@@ -5,12 +5,14 @@
 import { toggleVisibility } from "../helpers/Helper.js";
 
 export class GalleryView {
+    // Get gallery elements.
     constructor() {
         this.$galleryWindow = document.querySelector('.gallery__window');
         this.$galleryPlaceholder = document.querySelector('.gallery__placeholder');
         this.imgClass = 'gallery__item';
     }
 
+    // Insert new image into gallery window and add class.
     insertImage(image) {
         const img = document.createElement('img');
         img.src = image;
@@ -18,16 +20,19 @@ export class GalleryView {
         this.$galleryWindow.prepend(img);
     }
 
+    // Insert multiple images into gallery.
     insertImages(images) {
         images.forEach(image => {
             this.insertImage(image);
         });
     }
 
+    // Show or hide placeholder text.
     showPlaceholder(show) {
         toggleVisibility(this.$galleryPlaceholder, show);
     }
 
+    // Remove all images from gallery.
     clearGallery() {
         this.$galleryWindow.innerHTML = '';
         this.$galleryWindow.prepend(this.$galleryPlaceholder);
