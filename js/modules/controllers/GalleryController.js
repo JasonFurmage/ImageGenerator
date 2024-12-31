cdd1f88cc5aeeca08c64a2be480e8013fa05433b
JasonFurmage <150832588+JasonFurmage@users.noreply.github.com>
2024-12-31 01:34:10 +0000

    docs: add gallery controller comments

--- js/modules/controllers/GalleryController.js
+++ js/modules/controllers/GalleryController.js
@@ -18,11 +18,13 @@ export class GalleryController {
         document.addEventListener('image-count-change', this.#handleImageCountChange.bind(this));
     }
 
+    // Add new image into gallery.
     #handleAddImage(event) {
         const image = event.detail;
         this.view.insertImage(image);
     }
 
+    // Clear gallery and insert images for selected account.
     #handleAddImages(event) {
         const images = event.detail;
         const count = images.length
@@ -31,11 +33,13 @@ export class GalleryController {
         this.view.showPlaceholder(count === 0 ? true : false);
     }
 
+    // Show or hide placeholder depending on whether gallery is empty.
     #handleImageCountChange(event) {
         const count = event.detail;
         this.view.showPlaceholder(count === 0 ? true : false);
     }
 
+    // Remove all images and show placeholder.
     #handleClearImages() {
         this.view.clearGallery();
         this.view.showPlaceholder(true);
