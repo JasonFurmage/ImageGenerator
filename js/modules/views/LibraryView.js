08f0462591216ccbfd22b5c126684c4a664598cb
JasonFurmage <150832588+JasonFurmage@users.noreply.github.com>
2024-12-31 00:55:13 +0000

    docs: add library view comments

--- js/modules/views/LibraryView.js
+++ js/modules/views/LibraryView.js
@@ -5,6 +5,7 @@
 import { toggleVisibility, insertElementAtIndex } from "../helpers/Helper.js";
 
 export class LibraryView {
+    // Get library and home elements.
     constructor() {
         this.$libraryFull = document.querySelector('.library.library--full');
         this.$libraryMini = document.querySelector('.library.library--mini');
@@ -15,26 +16,31 @@ export class LibraryView {
         this.$gallery = document.querySelector('.gallery');
     }
 
+    // Switch between showing account select or account new.
     showLibrary(show) {
         toggleVisibility(this.$home, !show);
         toggleVisibility(this.$libraryFull, show);
     }
 
+    // Update library title with username.
     updateLibraryTitle(email) {
         const title = email ? `${this.#getUsername(email)}'s Library` : 'Library';
         this.$libraryTitle.textContent = title;
     }
 
+    // Include current image count on saved images button.
     updateHomeButtonTitle(count) {
         const title = count ? `View Saved Images (${count})` : 'View Saved Images';
         this.$homeButton.textContent = title;
     }
 
+    // Get username by truncating email address.
     #getUsername(email) {
         const username = email.split('@')[0];
         return username.charAt(0).toUpperCase() + username.slice(1);
     }
 
+    // Move gallery to full library or mini library depending on screen width.
     adjustGalleryPosition() {
         const width = window.innerWidth;
         const targetLibrary = width < 768 ? this.$libraryFull : this.$libraryMini;
